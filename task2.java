0fe21fe937beebeb7be25cfc87eb5810ae339c85
Utsav Rijal <your-email@example.com>
2025-12-26 13:08:15 +0545

    updated code

--- task2.java
+++ task2.java
@@ -1,5 +1,15 @@
 package tutorial07dec26;
 
 public class task2 {
+	public String name;  
+    public int age;      
 
+    public void Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Animal Name: " + name + ", Age: " + age);
+    }
 }
